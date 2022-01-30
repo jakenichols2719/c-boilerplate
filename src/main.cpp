84e5ca173026b3553a7283bb8ee2c3d40beafbc9
jnichols2719 <jnichols2719@gmail.com>
2022-01-29 19:53:10 -0700

    Removed test files

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "headers/test.h"
-#ifdef BUILD
-
-int main(int argc, char* argv[]) {
-    return 0;
-}
-
-#endif
--- src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "headers/test.h"
-
-int Calculator::add(int a, int b) {
-    return a+b;
-}
-
-int Calculator::sub(int a, int b) {
-    return a+b;
-}
